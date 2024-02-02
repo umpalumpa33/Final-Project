761e4b802d61a1cdee41cf034c6fcd99c4368f60
Ucha Sephiskveradze <ucha.sephiskveradze@gmail.com>
2024-02-03 02:41:12 +0400

    DZLIVS BICHOOO DZLIVS MIVXVDI SHECDM+ASJDIDJKLNDKLA

--- Javascript File/script.js
+++ Javascript File/script.js
@@ -47,3 +47,44 @@ function toggleModal() {
     modal.style.display = "none";
   }
 }
+
+function checkRequest() {
+  let nameInput = document.getElementById("nameInput");
+  let emailInput = document.getElementById("emailInput");
+  let phoneInput = document.getElementById("phoneInput");
+  let subjectInput = document.getElementById("subjectInput");
+  let messageInput = document.getElementById("messageInput");
+  let nameValue = nameInput.value;
+  let emailValue = emailInput.value;
+  let phoneValue = phoneInput.value;
+  let subjectValue = subjectInput.value;
+  let messageValue = messageInput.value;
+  let index = true;
+
+  function emailCheck(content) {
+    let pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return pattern.test(content);
+  }
+
+  function phoneCheck(content) {
+    let pattern = /^\+995/;
+    return pattern.test(content);
+  }
+
+  if (nameValue.trim() === "") {
+    alert(`Name input is mandatory!`);
+    index = false;
+  } else if (emailCheck(emailValue) == false) {
+    alert(`The email must be in the correct format `);
+    index = false;
+  } else if (phoneCheck(phoneValue) && phoneValue.length != 13) {
+    alert(`Phone number should start with +995 and be 12 digits`); // ამაზე შეკითხვა არ დამავიწყდეს (phoneCheck(phoneValue) == false
+    index = false;
+  } else if (subjectValue.length < 5) {
+    alert(`Subject should be more than 5 characters`);
+    index = false;
+  } else if (messageValue.trim() === "") {
+    alert(`Message is mandatory!`);
+    index = false;
+  }
+}
